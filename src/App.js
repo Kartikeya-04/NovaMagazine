1febe0b232959a0ca517a21ea2e72a715c14f010
Overlordddx03 <kartikpandey0604@gmail.com>
2024-02-09 19:30:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Grid from '@mui/material/Grid';
+// import './App.css';
+import './style.css';
+import One from './1.png';
+import Two from './2.png';
+import Three from './3.2.png';
+import Four from './4.png';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div>
+        <h1>NOVA MAGAZINE</h1>
+      </div>
+
+      <div className="main2">
+        <div className="border">
+          <div className="title">
+            <h2>Featured Magazines</h2>
+          </div>
+          <div className="title2">
+            <div className="hero">
+              <div className="one">
+                <img src={One} />
+              </div>
+              <div className="two">
+                <img src={Two} />
+              </div>
+              <div className="three">
+                <img src={Three} />
+              </div>
+              <div className="four">
+                <img src={Four} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
